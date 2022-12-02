6464d33e8cf5310e20c5563791059dbdeb598e96
cybersen <gashnya_official@bk.ru>
2022-12-02 09:28:10 +0300

    Ver 1.0 alpha - just created project

---
+++ wallet/accounts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+from .managers import CustomUserManager
+
+
+import uuid #unique ID`es
+
+# Create your models here.
