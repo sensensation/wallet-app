d9bda6d8bd7d91f3d800744896f86968b89d4a27
cybersen <gashnya_official@bk.ru>
2022-12-02 09:59:34 +0300

    Ver 1.1 alpha - model user

--- wallet/accounts/models.py
+++ wallet/accounts/models.py
@@ -8,4 +8,23 @@ from .managers import CustomUserManager
 
 import uuid #unique ID`es
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+   """
+   Creates a new users
+   """
+   USERNAME_FIELD = "email"
+   REQUIRED_FIELDS = []
+
+   username = None
+   uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+   email = models.EmailField(_("email address"), blank=False, unique=True)
+   first_name = models.CharField(_("first name"), max_length=150, blank=False)
+   last_name = models.CharField(_("last name"), max_length=150, blank=False)
+   date_of_birth = models.DateField(_("date of birth"), max_length=150, blank=False)
+   verified = models.BooleanField(_("verified"), default=False)
+
+   objects = CustomUserManager()
+
+   def __str__(self):
+        return self.email
--- wallet/wallet/settings.py
+++ wallet/wallet/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "transactions",
 ]
 
+AUTH_USER_MODEL = "accounts.CustomUser"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
