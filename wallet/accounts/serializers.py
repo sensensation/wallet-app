af223f9798441095a0162d9badf147f7524fa055
cybersen <gashnya_official@bk.ru>
2022-12-03 11:10:11 +0300

    Ver 2a - backup: create users, login JWT, List of users

---
+++ wallet/accounts/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class UserListSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = CustomUser
+      fields = ('__all__')
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = CustomUser
+      fields = ('__all__')
+
+   def create(self, validated_data):
+        user = CustomUser.objects.create(
+            email = validated_data['email'],
+            password = validated_data['password'],
+            date_of_birth = validated_data['date_of_birth'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
