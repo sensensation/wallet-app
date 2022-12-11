5158a1fc46eb0fb0298cc0b297681d6e05f02612
cybersen <gashnya_official@bk.ru>
2022-12-11 15:05:11 +0300

    Ver 3.5c - balance app tests added

--- wallet/tests/pytests/conftest.py
+++ wallet/tests/pytests/conftest.py
@@ -31,21 +31,38 @@ def user_data_fixture():
 
 @pytest.fixture
 def user_login():
-    user = {
+   """
+   Creates and logining new user
+   """
+   user = {
         'password':'bebrauser',
          'email':'test_email@gmail.com',
          'first_name':'test',
          'last_name':'user',
          'date_of_birth':'2000-01-01'
     }
-    client.post('http://127.0.0.1:8000/register/', user)
+   client.post('http://127.0.0.1:8000/register/', user)
 
-    response = client.post('http://127.0.0.1:8000/api/token/', 
+   response = client.post('http://127.0.0.1:8000/api/token/', 
                             dict(email = user['email'],
                                 password = user['password']))
 
-    return response
+   return response
  
+@pytest.fixture
+def create_wallet_fixture(user_login):
+   """
+   Creates a new wallet
+   """
+   wallet_data = {
+        "currency":"RUB",
+        "card_type":"MasterCard"
+        }
+   token = user_login.data['access'] 
+   url = 'http://127.0.0.1:8000/create_wallet/'
+   response = client.post(url, wallet_data, HTTP_AUTHORIZATION='JWT {}'.format(token))
+   return response
+
 @pytest.fixture
 def wallet_data_fixture():
    """
--- wallet/tests/pytests/test_balance.py
+++ wallet/tests/pytests/test_balance.py
@@ -4,18 +4,11 @@ from rest_framework.test import APIClient
 client = APIClient()
 
 @pytest.mark.django_db
-def test_create_wallet(wallet_data_fixture, user_login):
+def test_create_wallet(create_wallet_fixture, user_login):
    """
    Wallet creation behavior verification
    """
-   url = 'http://127.0.0.1:8000/create_wallet/'
-   
-   token = user_login.data['access'] 
-   
-   response = client.post(url, wallet_data_fixture, HTTP_AUTHORIZATION='JWT {}'.format(token))
-   
-   data = response.data 
-
+   data = create_wallet_fixture.data 
    assert user_login.status_code == 200
    assert 'access' in user_login.data 
    assert 'refresh' in user_login.data 
@@ -27,24 +20,68 @@ def test_create_wallet(wallet_data_fixture, user_login):
    assert "updated_at" in data
    assert "balance" in data
    assert "user" in data
-   assert response.status_code == 201
+   assert create_wallet_fixture.status_code == 201
 
 @pytest.mark.django_db
-def test_bad_create_wallet(wallet_data_fixture, user_login):
-   pass
+def test_bad_create_wallet(user_login):
+   """
+   Wallet invalid creation behavior verification
+   """
+   url = 'http://127.0.0.1:8000/create_wallet/'
+   token = user_login.data['access'] 
+   wallet_data = {
+        "currency":"TUGRIKI",
+        "card_type":"BebraCards"
+        }
+   response = client.post(url, wallet_data, HTTP_AUTHORIZATION='JWT {}'.format(token))
+   assert response.status_code == 400
 
 @pytest.mark.django_db
-def test_get_wallet_balance(wallet_data_fixture, user_login):
-   pass
+def test_get_user_wallet(user_login):
+   """
+   Getting concrete user`s wallets behavior verification
+   """
+   token = user_login.data['access'] 
+   url = 'http://127.0.0.1:8000/wallets/'
+   
+   response = client.get(url, HTTP_AUTHORIZATION='JWT {}'.format(token))
 
+   assert response.status_code == 200
+   
 @pytest.mark.django_db
-def test_get_user_wallet(wallet_data_fixture, user_login):
-   pass
+def test_get_wallet_balance(create_wallet_fixture, user_login):
+   """
+   Getting concrete wallet`s balance
+   """
+   token = user_login.data['access'] 
+   
+   wallet = create_wallet_fixture.data['uid']
+   url = f'http://127.0.0.1:8000/balance/{wallet}'
+
+   response = client.get(url, HTTP_AUTHORIZATION='JWT {}'.format(token))
+
+   assert response.status_code == 200
 
 @pytest.mark.django_db
 def test_wallets_amount(wallet_data_fixture, user_login):
-   pass
+   """
+   Checking the possibility of creating no more than 5 wallets
+   """
+   token = user_login.data['access'] 
+   url = 'http://127.0.0.1:8000/create_wallet/'
+   for i in range (0,6):
+      response = client.post(url, wallet_data_fixture, HTTP_AUTHORIZATION='JWT {}'.format(token))
+   
+   assert "Too much wallets you have!" in response.data
 
 @pytest.mark.django_db
-def test_wallets_currency(wallet_data_fixture, user_login):
-   pass
+def test_wallets_currency(create_wallet_fixture):
+   """
+   Checking accruals in accordance with the currency
+   """
+   balance = create_wallet_fixture.data['balance']
+   currency = create_wallet_fixture.data['currency']
+   if currency == 'RUB':
+      assert balance == '100.00'
+   else:
+      assert balance == '3.0'
